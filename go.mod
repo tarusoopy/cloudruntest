167fc9229efe9aac622c0b7bb1b29e6e934804ba
h.nakano <h.nakano@variosecure.net>
2021-08-24 17:00:19 +0900

    Simple HTTP Server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tarusoopy/cloudruntest
 
 go 1.15
+
+require github.com/valyala/fasthttp v1.29.0
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"github.com/valyala/fasthttp"
+)
+
 func main() {
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		fmt.Fprintf(ctx, "hello fasthttp")
+	}
+	s := &fasthttp.Server{
+		Handler: requestHandler,
+		Name:    "PC Register API Server",
+	}
+	address := "0.0.0.0" + ":" + "28080"
+	if err := s.ListenAndServe(address); err != nil {
+		fmt.Errorf("error in ListenAndServe: %s", err)
+	}
 
 }
