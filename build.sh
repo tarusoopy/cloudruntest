7cee50923e65135d5028ff68c33bf08d7c2a325c
h.nakano <h.nakano@variosecure.net>
2021-08-27 14:16:02 +0900

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# gcloud builds submit --config CONFIG_FILE_PATH SOURCE_DIRECTORY
+gcloud builds submit --config
+
+## Use Dockerfile build
+# gcloud builds submit --tag gcr.io/sdns-prd-273508/cloudruntest
+
+## Use Cloud Native Buildpack
+## gcloud builds submit --pack builder=BUILDPACK_BUILDER, env=ENVIRONMENT_VARIABLE, image=IMAGE
+# gcloud builds submit --pack image=gcr.io/cloudruntest
